e021e95fc6ff1f55fbe0813db644c269afcd3c7e
izalzl5 <faizalizzudin18@gmail.com>
2024-04-27 19:19:16 +0700

    intial commit

--- lib/presentation/screens/SplashScreen.dart
+++ lib/presentation/screens/SplashScreen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: TweenAnimationBuilder(
+          duration: Duration(milliseconds: 800),
+          tween: Tween<double>(begin: 0, end: 1),
+          builder: (BuildContext context, double value, Widget? child) {
+            return AnimatedOpacity(
+              duration: value < 0.2 ? Duration(milliseconds: 200) : Duration(milliseconds: 100),
+              opacity: value,
+              child: Image.asset(
+                'lib/assets/icons/logo.png',
+                fit: BoxFit.cover,
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
