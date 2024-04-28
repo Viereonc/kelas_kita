ca912e3cd44edc8f57c3443c9ebf29868bf17a75
farifauzi <farisfauzi444@gmail.com>
2024-04-28 20:23:16 +0700

    Login Register

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -2,6 +2,8 @@ import 'package:get/get.dart';
 import 'package:kelas_kita/presentation/screens/SplashScreen.dart';
 import 'package:kelas_kita/presentation/screens/home/home_view.dart';
 import 'package:kelas_kita/presentation/screens/onboarding/onboarding_view.dart';
+import 'package:kelas_kita/presentation/registration/login_view.dart';
+import 'package:kelas_kita/presentation/registration/register_view.dart';
 import 'package:kelas_kita/routes/app_routes.dart';
 
 class AppPages{
@@ -25,6 +27,16 @@ class AppPages{
     GetPage(
         name: Path.HOMESCREEN_PAGE,
         page: () => HomeScreen(),
+        transition: Transition.noTransition
+    ),
+    GetPage(
+        name: Path.LOGINVIEW_PAGE,
+        page: () => LoginView(),
+        transition: Transition.noTransition
+    ),
+    GetPage(
+        name: Path.REGISTERVIEW_PAGE,
+        page: () => RegisterView(),
         transition: Transition.noTransition
     ),
 
